7351406e25f90811c923752507a48fd784fbfdd6
Poonam <poonambkanani@gmail.com>
2022-05-09 02:42:10 -0700

    final version of functions

---
+++ HW2/functions/slack-interactive/handler.py
@@ -0,0 +1,31 @@
+import json
+import urllib
+
+def handle(req):
+    urlstring = urllib.unquote(req).decode('utf8').strip('payload=')
+    response = json.loads(urlstring)
+    inp = response["actions"][0]["value"]
+    selected = "You selected Of course!" if inp == 'recommend' else "You selected most Definitely!"
+    data = {
+        "attachments": [
+            {
+                "replace_original": True,
+                "response_type": "ephemeral",
+                "fallback": "Required plain-text summary of the attachment.",
+                "color": "#36a64f",
+                "pretext": selected + "Ahh yeah! Great choice, COEN 241 is absolutely amazing!",
+                "author_name": "Poonam",
+                "author_link": "https://github.com/pbkanani/COEN241_CC",
+                "author_icon": "https://avatars.githubusercontent.com/u/92608195?s=400&v=4",
+                "title": "COEN 241",
+                "title_link": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "text": "Head over to COEN 241",
+                "image_url": "https://www.scu.edu/media/offices/umc/scu-brand-guidelines/visual-identity-amp-photography/visual-identity-toolkit/logos-amp-seals/Mission-Dont3.png",
+                "thumb_url": "https://www.scu.edu/engineering/academic-programs/department-of-computer-engineering/graduate/course-descriptions/",
+                "footer": "Slack Apps built on OpenFaas",
+                "footer_icon": "https://a.slack-edge.com/45901/marketing/img/_rebrand/meta/slack_hash_256.png",
+                "ts": 123456789
+            }
+        ]
+    }
+    return json.dumps(data)
